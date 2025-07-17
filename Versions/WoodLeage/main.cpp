227e837c174b4ef9208ee422a73d3963e4c582e3
Robertas <Robertas@Mackus.lt>
2025-07-17 16:03:44 +0300

    WoodLeage4

---
+++ Versions/WoodLeage/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Win the water fight by controlling the most territory, or out-soak your opponent!
+ **/
+
+int main()
+{
+    int my_id; // Your player id (0 or 1)
+    cin >> my_id; cin.ignore();
+    int agent_count; // Total number of agents in the game
+    cin >> agent_count; cin.ignore();
+    for (int i = 0; i < agent_count; i++) {
+        int agent_id; // Unique identifier for this agent
+        int player; // Player id of this agent
+        int shoot_cooldown; // Number of turns between each of this agent's shots
+        int optimal_range; // Maximum manhattan distance for greatest damage output
+        int soaking_power; // Damage output within optimal conditions
+        int splash_bombs; // Number of splash bombs this can throw this game
+        cin >> agent_id >> player >> shoot_cooldown >> optimal_range >> soaking_power >> splash_bombs; cin.ignore();
+    }
+    int width; // Width of the game map
+    int height; // Height of the game map
+    cin >> width >> height; cin.ignore();
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int x; // X coordinate, 0 is left edge
+            int y; // Y coordinate, 0 is top edge
+            int tile_type;
+            cin >> x >> y >> tile_type; cin.ignore();
+        }
+    }
+
+    // game loop
+    while (1) {
+        int agent_count;
+        cin >> agent_count; cin.ignore();
+        for (int i = 0; i < agent_count; i++) {
+            int agent_id;
+            int x;
+            int y;
+            int cooldown; // Number of turns before this agent can shoot
+            int splash_bombs;
+            int wetness; // Damage (0-100) this agent has taken
+            cin >> agent_id >> x >> y >> cooldown >> splash_bombs >> wetness; cin.ignore();
+        }
+        int my_agent_count; // Number of alive agents controlled by you
+        cin >> my_agent_count; cin.ignore();
+        for (int i = 0; i < my_agent_count; i++) {
+
+            // Write an action using cout. DON'T FORGET THE "<< endl"
+            // To debug: cerr << "Debug messages..." << endl;
+
+
+            // One line per agent: <agentId>;<action1;action2;...> actions are "MOVE x y | SHOOT id | THROW x y | HUNKER_DOWN | MESSAGE text"
+            
+        }
+        cout << "1;MOVE 6 1"<< endl;
+        cout << "2;MOVE 6 3"<< endl;
+    }
+}
